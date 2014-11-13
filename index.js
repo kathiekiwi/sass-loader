e3d4eba44bb7db4a5ab428a83b22b27ae8e5a770
Katherine Kiwi <live-evil@yandex.ru>
2014-11-13 16:34:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ module.exports = function (content) {
           delete opt.data;
           opt.file = info.path;
           sass.render(opt);
-          temp.cleanupSync();
+          //temp.cleanupSync();
         });
       });
     } else {
